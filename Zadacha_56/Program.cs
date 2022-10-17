6eb8993063fd55b3389fd6efb72484d329a15fe7
Yujev <ustatar@ya.ru>
2022-10-17 18:49:22 +0300

    Решил задачу №56

---
+++ Zadacha_56/Program.cs
@@ -0,0 +1,82 @@
+﻿/* Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить
+ строку с наименьшей суммой элементов.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей
+ суммой элементов: 1 строка 
+ */
+Console.Clear();
+Console.WriteLine();
+Console.Write("Введи количество строк: \n");
+int rows = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("Введи количество столбцов: \n");
+int columns = Convert.ToInt32(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, columns, 0, 10);
+Console.WriteLine("Твой массив: ");
+Console.WriteLine();
+PrintArray(array);
+
+Console.WriteLine();
+Console.WriteLine($" {GetRowNumber(array)}-я строка с наименьшей суммой элементов. (Нумерация начинается с нуля).");
+Console.WriteLine();
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int GetRowNumber(int[,] array)
+{
+    int row = 0;
+    int minsum = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minsum = minsum + array[0, i];
+    }
+
+    for (int i = 1; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum = sum + array[i, j];
+        }
+        if (minsum > sum)
+        {
+            minsum = sum;
+            row = i;
+        }
+    }
+    return row;
+}
+
+
